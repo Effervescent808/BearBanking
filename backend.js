4a4673f6a0f9825e42c0bfb421a72a34453beb61
Tyson Haufschild <thaufschild@hawk.illinoistech.edu>
2026-04-03 17:20:51 -0500

    update for railway

--- backend.js
+++ backend.js
@@ -7,6 +7,7 @@ async function startServer() {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(express.static('.'));
 
   // open SQLite database file
   const db = await open({
@@ -97,9 +98,8 @@ async function startServer() {
   });
 
   // CREATES SERVER
-  app.use(express.static('.'));
-  app.listen(3000, () => console.log("Server running at http://localhost:3000"));
-}
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Running on ${PORT}`));}
 
 // Start the server and catch any errors
 startServer().catch(err => {
